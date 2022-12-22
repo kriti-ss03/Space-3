dfd9aa8b1b4d1ebb37ef5a825eb4f2222c00700b
kriti2020 <96840464+kriti2020@users.noreply.github.com>
2022-12-23 01:26:22 +0530

    sound

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import WebGL from './WebGL';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+
 const scene = new THREE.Scene();
 //camera- field wiew,aspect ratio,view frustum==most common data based on eyeball persepective
 
@@ -58,14 +59,14 @@ function addStar() {
 Array(200).fill().forEach(addStar);
 
 //SPACE
-const spaceTexture = new THREE.TextureLoader().load('sp.jpg');
+const spaceTexture = new THREE.TextureLoader().load('/img/sp.jpg');
 scene.background = spaceTexture;
 
 //CUBE TEXTURE & MESH(=GEOMETRY+MATERIAL)
     //const meTexture = new THREE.TextureLoader().load('/img/one.jpg');
     //const me = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: meTexture }));
 //ADDING DIFF TEXT ON EACH SIDE
-const me_geo = new THREE.BoxGeometry(3, 3, 3);
+const me_geo = new THREE.BoxGeometry(8, 8, 8);
 const me_mat = [
   new THREE.MeshBasicMaterial({ map:  new THREE.TextureLoader().load("/img/one.jpg") }),//x +ve
 
@@ -90,23 +91,125 @@ const moonTexture = new THREE.TextureLoader().load('/img/moon.png');
 const normalTexture = new THREE.TextureLoader().load('/img/normal.jpg');
 
 const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.SphereGeometry(3, 16, 16),
   new THREE.MeshStandardMaterial({
     map: moonTexture,
    normalMap: normalTexture,
   })
 );
 
+const neptTexture = new THREE.TextureLoader().load('/img/nept.jpeg');
+//const neptex = new THREE.TextureLoader().load('/img/normal.jpg');
+
+const nept = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 16, 16),
+  new THREE.MeshStandardMaterial({
+    map: neptTexture,
+  //  normalMap: normalTexture,
+  })
+);
+
+const marsTexture = new THREE.TextureLoader().load('/img/mars.jpeg');
+
+const mars = new THREE.Mesh(
+  new THREE.SphereGeometry(4, 32, 32),
+  new THREE.MeshBasicMaterial({
+    map: marsTexture
+  })
+);
+
+const jupTexture = new THREE.TextureLoader().load('/img/jupiter.jpg');
+
+const jup = new THREE.Mesh(
+  new THREE.SphereGeometry(10, 120, 120),
+  new THREE.MeshStandardMaterial({
+    map: jupTexture,
+   normalMap: normalTexture,
+  })
+);
+
+const uranTexture = new THREE.TextureLoader().load('/img/uran.jpg');
+
+const uran= new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: uranTexture,
+   normalMap: normalTexture,
+  })
+);
+
+scene.add(nept);
+scene.add(mars);
+scene.add(jup);
+scene.add(uran);
 scene.add(moon);
+
+
+const g1Texture = new THREE.TextureLoader().load('/img/g1.jpeg');
+const g1 = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: g1Texture }));
+scene.add(g1);
+
+const g2Texture = new THREE.TextureLoader().load('/img/g2.jpeg');
+const g2 = new THREE.Mesh(new THREE.BoxGeometry(4, 4, 4), new THREE.MeshBasicMaterial({ map: g2Texture }));
+scene.add(g2);
+
+const g3Texture = new THREE.TextureLoader().load('/img/g3.jpeg');
+const g3 = new THREE.Mesh(new THREE.BoxGeometry(4, 4, 4), new THREE.MeshBasicMaterial({ map: g3Texture }));
+scene.add(g3);
+
+const g4Texture = new THREE.TextureLoader().load('/img/g4.jpeg');
+const g4 = new THREE.Mesh(new THREE.BoxGeometry(4, 4, 4), new THREE.MeshBasicMaterial({ map: g4Texture }));
+scene.add(g4);
+
+const g5Texture = new THREE.TextureLoader().load('/img/g5.jpeg');
+const g5 = new THREE.Mesh(new THREE.BoxGeometry(6, 6, 6), new THREE.MeshBasicMaterial({ map: g5Texture }));
+scene.add(g5);
+
+
 
 //POSITION
 moon.position.z = 25;
 moon.position.setX(-12);
 moon.position.setY(-4);
 
-me.position.z = -5;
-me.position.x = 2;
+nept.position.z = 10;
+nept.position.setX(-18);
+nept.position.setY(14);
 
+mars.position.z = -25;
+mars.position.setX(49);
+mars.position.setY(40);
+
+jup.position.z = 6;
+jup.position.setX(28);
+jup.position.setY(-24);
+
+uran.position.z = -4;
+uran.position.setX(-10);
+uran.position.setY(-30);
+
+
+me.position.z = -2;
+
+g1.position.z = -8;
+g1.position.x = 28;
+g1.position.y = 8;
+
+g2.position.z = -16;
+g2.position.x = -29;
+g2.position.y = 10;
+
+g3.position.z = 18;
+g3.position.x = -30;
+g3.position.y = -20;
+
+g4.position.z = 8;
+g4.position.x = 3;
+g4.position.y = -18;
+
+g5.position.z = 18;
+g5.position.x = 15;
+g5.position.y = 22;
 
 
 
@@ -146,7 +249,20 @@ function moveCamera() {
 // document.body.onscroll = moveCamera;
 // moveCamera();
 
+const listener = new THREE.AudioListener();
+camera.add(listener);
 
+const audioLoader = new THREE.AudioLoader();
+
+const backgroundSound = new THREE.Audio(listener);
+
+audioLoader.load('/sounds/audio.mp3',
+  function (buffer) {
+  backgroundSound.setBuffer(buffer);
+  backgroundSound.setLoop(true);
+  backgroundSound.setVolume(0.4);
+  backgroundSound.play();
+});
 //as we need renderer to run again so we r creating infinte loop to avoid multiple times writing that
 function animate() {
   requestAnimationFrame(animate);
@@ -156,10 +272,18 @@ function animate() {
   torus.rotation.z += 0.01;
 
   controls.update(); //to update controls-mouse controlling 
-  
+  backgroundSound.play();
   renderer.render(scene, camera);
+
 }
 
 animate();
 
+
+
+//AUDIO
+
+// var noise = new SimpleNoise();
+// var audio = new Audio("/sounds/audio.mp3");
+// var play = false;
 
